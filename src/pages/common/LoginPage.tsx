1aa213645857285448f3574eb83d8d119234b9f3
ajiaja38 <ajicooljazz38@gmail.com>
2025-05-29 10:59:38 +0700

    feat: update minor

--- src/pages/common/LoginPage.tsx
+++ src/pages/common/LoginPage.tsx
@@ -1,7 +1,61 @@
 import React, { type JSX } from "react"
+import { Button, Form, Input } from "antd"
+import type { FormProps } from "antd"
+
+type FieldType = {
+  username?: string
+  password?: string
+}
+
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+  console.log("Success:", values)
+}
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo)
+}
 
 const LoginPage: React.FC = (): JSX.Element => {
-  return <div>LoginPage</div>
+  return (
+    <div className="h-screen bg-slate-200 flex flex-col justify-center items-center">
+      <div className="p-3 bg-white rounded-xl w-[30rem] shadow-xl px-7">
+        <h1 className="font-bold text-center text-xl">Silahkan Masuk</h1>
+        <Form
+          name="basic"
+          layout="vertical"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: "Password wajib diisi!" },
+              { min: 6, message: "Password minimal 6 karakter!" },
+            ]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+
+          <Form.Item label={null}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
