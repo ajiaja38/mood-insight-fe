f71bc6a61939222b5fba8616de931d1eee5e7d82
ajiaja38 <ajicooljazz38@gmail.com>
2025-06-02 22:06:45 +0700

    feat: update detail diagnosis result :zap:

--- src/pages/private/DetailDiagnosisResult.tsx
+++ src/pages/private/DetailDiagnosisResult.tsx
@@ -1,10 +1,37 @@
-import React, { type JSX } from "react"
+import React, { useEffect, type JSX } from "react"
 import UseTitle from "../../hooks/useTitle"
 import BreadCrumb from "../../components/Breadcumb"
 import { useParams } from "react-router-dom"
+import { App } from "antd"
+import { useQuery } from "@tanstack/react-query"
+import type { IDetailConsultation } from "../../types/interface/IConsultation.interface"
+import { ConsultationService } from "../../service/consultation.service"
 
 const DetailDiagnosisResult: React.FC = (): JSX.Element => {
   const { id } = useParams()
+
+  const { notification } = App.useApp()
+
+  const { data, error } = useQuery({
+    queryKey: ["detail-diagnosis-result", id],
+    queryFn: (): Promise<IDetailConsultation> =>
+      ConsultationService.getDetailConsultation(id as string).then(
+        (res) => res.data
+      ),
+  })
+
+  if (error) {
+    notification.error({
+      message: "Error",
+      description: error.message,
+    })
+  }
+
+  useEffect(() => {
+    if (data) {
+      console.log(data)
+    }
+  }, [data])
 
   return (
     <>
