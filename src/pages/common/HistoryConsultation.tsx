d2c80d704d9354b3ac142b00946c938de66f5250
ajiaja38 <ajicooljazz38@gmail.com>
2025-06-08 19:56:33 +0700

    feat: success add history consultation user :art:

--- src/pages/common/HistoryConsultation.tsx
+++ src/pages/common/HistoryConsultation.tsx
@@ -1,11 +1,90 @@
-import React, { type JSX } from "react"
+import React, { useEffect, type JSX } from "react"
 import UseTitle from "../../hooks/useTitle"
+import { useQuery } from "@tanstack/react-query"
+import { App, Button, type TableProps } from "antd"
+import type { IConsultation } from "../../types/interface/IConsultation.interface"
+import { ConsultationService } from "../../service/consultation.service"
+import type { ResponseEntity } from "../../types/interface/IResponse.interface"
+import { timeIdFormat } from "../../utils/pipe/timeIdFormat.pipe"
+import { NavLink } from "react-router-dom"
+import Container from "../../components/Content/Container"
+import ContainerTable from "../../components/Content/ContainerTable"
 
 const HistoryConsultation: React.FC = (): JSX.Element => {
+  const { notification } = App.useApp()
+
+  const { data, error } = useQuery({
+    queryKey: ["diagnosis-result"],
+    queryFn: (): Promise<IConsultation[]> =>
+      ConsultationService.getConsultation(true).then(
+        (res: ResponseEntity<IConsultation[]>) => res.data
+      ),
+  })
+
+  if (error)
+    notification.error({
+      message: "Error",
+      description: error.message,
+    })
+
+  const columns: TableProps<IConsultation>["columns"] = [
+    {
+      title: "Nama Pengguna",
+      dataIndex: "user",
+      key: "user",
+      sorter: (a, b): number => a.user.localeCompare(b.user),
+      ellipsis: {
+        showTitle: false,
+      },
+    },
+    {
+      title: "Hasil",
+      dataIndex: "result",
+      key: "result",
+      sorter: (a, b): number => a.result.localeCompare(b.result),
+      ellipsis: {
+        showTitle: false,
+      },
+    },
+    {
+      title: "Tanggal Konsultasi",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      sorter: (a, b): number => a.createdAt.localeCompare(b.createdAt),
+      ellipsis: {
+        showTitle: false,
+      },
+      render: (_, { createdAt }) => timeIdFormat(new Date(createdAt)),
+    },
+    {
+      title: "Aksi",
+      key: "action",
+      render: (_, record): JSX.Element => (
+        <NavLink to={`/detail-konsultasi/${record.id}`}>
+          <Button color="primary" variant="solid">
+            Detail
+          </Button>
+        </NavLink>
+      ),
+    },
+  ]
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div className="bg-gray-100 content-padding">
       <UseTitle title="Riwayat Konsultasi" />
-      <div className="container mx-auto"></div>
+      <div className="container mx-auto">
+        <Container>
+          <ContainerTable<IConsultation>
+            title="Daftar Hasil Diagnosa"
+            columns={columns}
+            data={data}
+          />
+        </Container>
+      </div>
     </div>
   )
 }
