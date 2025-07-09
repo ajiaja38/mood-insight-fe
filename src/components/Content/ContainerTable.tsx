8a56779c6581e5e03031adf513ee0e6643cb2b98
ajiaja38 <ajicooljazz38@gmail.com>
2025-07-09 10:28:36 +0700

    feat: upate minor :fire:

--- src/components/Content/ContainerTable.tsx
+++ src/components/Content/ContainerTable.tsx
@@ -1,18 +1,16 @@
-import { useState, type JSX } from "react"
-import { Button, Table, Typography, type TableProps } from "antd"
-import { AppstoreAddOutlined } from "@ant-design/icons"
-import EditableCell from "../Editable/EditableCell"
-import EditableRow from "../Editable/EditableRow"
+import { useState, type JSX } from 'react';
+import { Button, Table, Typography, type TableProps } from 'antd';
+import { AppstoreAddOutlined } from '@ant-design/icons';
+import EditableCell from '../Editable/EditableCell';
+import EditableRow from '../Editable/EditableRow';
 
 interface props<T> {
-  title: string
-  data: T[] | undefined
-  columns: TableProps<T>["columns"]
-  isAdd?: boolean
-  callback?: () => void
+  title: string;
+  data: T[] | undefined;
+  columns: TableProps<T>['columns'];
+  isAdd?: boolean;
+  callback?: () => void;
 }
-
-const { Title } = Typography
 
 const ContainerTable = <T,>({
   title,
@@ -21,15 +19,15 @@ const ContainerTable = <T,>({
   isAdd,
   callback,
 }: props<T>): JSX.Element => {
-  const [pageSize, setPageSize] = useState<number>(5)
+  const [pageSize, setPageSize] = useState<number>(5);
 
   return (
     <>
-      <div className="flex flex-col md:flex-row md:justify-between">
-        <Title level={3}>{title}</Title>
+      <div className='flex flex-col md:flex-row md:justify-between'>
+        <Typography.Title level={3}>{title}</Typography.Title>
         {isAdd ? (
           <Button
-            type="primary"
+            type='primary'
             icon={<AppstoreAddOutlined />}
             onClick={callback}
           >
@@ -46,22 +44,22 @@ const ContainerTable = <T,>({
         }}
         columns={columns}
         dataSource={data}
-        showSorterTooltip={{ target: "sorter-icon" }}
+        showSorterTooltip={{ target: 'sorter-icon' }}
         pagination={{
           pageSize,
           showSizeChanger: true,
           onShowSizeChange: (_current, size) => setPageSize(size),
-          pageSizeOptions: ["5", "10", "20", "50"],
-          position: ["bottomRight"],
+          pageSizeOptions: ['5', '10', '20', '50'],
+          position: ['bottomRight'],
           showTotal: (total, range) =>
             `${range[0]}-${range[1]} dari ${total} data`,
         }}
-        size="small"
+        size='small'
         rowKey={(record) => (record as any).id}
         bordered
       />
     </>
-  )
-}
+  );
+};
 
-export default ContainerTable
+export default ContainerTable;
