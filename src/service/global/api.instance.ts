e58c1604bf6e4626edbd4371b95de278c1d02900
ajiaja38 <ajicooljazz38@gmail.com>
2025-07-21 18:54:21 +0700

    feat: update minor :zap:

--- src/service/global/api.instance.ts
+++ src/service/global/api.instance.ts
@@ -2,7 +2,7 @@ import type { AxiosInstance } from "axios"
 import axios from "axios"
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "https://mood-insight-be.vercel.app/v1",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
