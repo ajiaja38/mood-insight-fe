db79e04b2f2f93afb95b3e77249cbeb11e1989a5
ajiaja38 <ajicooljazz38@gmail.com>
2025-07-21 19:01:14 +0700

    feat: udpate base url :zap:

--- src/service/global/api.instance.ts
+++ src/service/global/api.instance.ts
@@ -2,7 +2,7 @@ import type { AxiosInstance } from "axios"
 import axios from "axios"
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: "https://mood-insight-be.vercel.app/v1",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,4 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://mood-insight-be.vercel.app/v1",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-  preview: {
-    proxy: {
-      "/api": {
-        target: "https://mood-insight-be.vercel.app/v1",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 })
